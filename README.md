86b34b4da241565c432e40a3282803ffc357f012
LiDing0611 <liding_0611@163.com>
2019-05-20 17:14:12 +0800

    update commit

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
-# threadpool
+# A C++ High Performance Thread Pool
+
+[![Build Status](https://travis-ci.org/linyacool/WebServer.svg?branch=master)](https://travis-ci.org/linyacool/WebServer)
+[![license](https://img.shields.io/github/license/mashape/apistatus.svg)](https://opensource.org/licenses/MIT)
+
+  
+## Introduction  
+
+本项目为C++11编写的线程池
+
+
+## Envoirment  
+* OS: Ubuntu 16.04
+* Complier: g++ 5.4
+
+## Build
+
+	make
+
+## Usage
+	./threadpool [-t thread_numbers]  
+
+## Technical points
+* cond 条件变量  
+* cond 条件变量  
+* pthread 使用
--- main.cpp
+++ main.cpp
@@ -1,12 +1,10 @@
-﻿#define THREADPOOL_THREAD_NUM 32
-#define QUEUE_SIZE  256
-
+﻿#define QUEUE_SIZE  256
+#include <stdlib.h>
 #include <pthread.h>
 #include "threadpool.h"
 #include <iostream>
+#include <stdlib.h>
 using namespace std;
-
-int tasks = 0, done = 0;
 
 
 void run_thread(void *arg) {
@@ -17,11 +15,15 @@ int main(int argc, char **argv)
 {
 
 
+	if(argc != 2){
+		cout<<"Usage: ./threadpool [-t thread_numbers]";
+		exit(1);
+	}
+	int THREADPOOL_THREAD_NUM = atoi(argv[2]);
 	threadpool_t *threadpool = threadpool_create(THREADPOOL_THREAD_NUM, QUEUE_SIZE, 0);
 	if(threadpool !=NULL){
 		cout << "Threadpool started with " << THREADPOOL_THREAD_NUM << " and queue size of " << QUEUE_SIZE<<endl;
 	}
-
 
 	while (threadpool_add(threadpool, &run_thread, NULL, 0) == 0) {
 		
